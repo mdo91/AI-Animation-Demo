3d378c64a511330b673316bed80ce0a61ed6be99
Mahmut Aoata <hesoka_mxx@yahoo.com>
2026-05-26 05:19:08 +0300

    Initial Commit

---
+++ AI-Animation-Demo/AI_Animation_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Animation_DemoApp.swift
+//  AI-Animation-Demo
+//
+//  Created by Mahmoud Aoata on 26/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct AI_Animation_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
